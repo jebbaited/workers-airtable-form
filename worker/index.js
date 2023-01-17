9e75f1c8ad5e855e34e0371121aa906077901df8
jebbaited <kostadrapak@gmail.com>
2023-01-17 22:07:36 +0200

    changes

--- worker/index.js
+++ worker/index.js
@@ -3,24 +3,29 @@ addEventListener('fetch', event => {
 })
 
 const createAirtableRecord = body => {
-  return fetch(`https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(AIRTABLE_TABLE_NAME)}`, {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: {
-      Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-      'Content-type': `application/json`
-    }
-  })
+  return fetch(
+    `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(
+      AIRTABLE_TABLE_NAME,
+    )}`,
+    {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+        'Content-type': `application/json`,
+      },
+    },
+  )
 }
 
 const submitHandler = async request => {
-  if (request.method !== "POST") {
-    return new Response("Method Not Allowed", {
-      status: 405
+  if (request.method !== 'POST') {
+    return new Response('Method Not Allowed', {
+      status: 405,
     })
   }
 
-  const body = await request.formData();
+  const body = await request.formData()
 
   const {
     first_name,
@@ -28,18 +33,18 @@ const submitHandler = async request => {
     email,
     phone,
     subject,
-    message
+    message,
   } = Object.fromEntries(body)
 
   const reqBody = {
     fields: {
-      "First Name": first_name,
-      "Last Name": last_name,
-      "Email": email,
-      "Phone Number": phone,
-      "Subject": subject,
-      "Message": message
-    }
+      'First Name': first_name,
+      'Last Name': last_name,
+      Email: email,
+      'Phone Number': phone,
+      Subject: subject,
+      Message: message,
+    },
   }
 
   await createAirtableRecord(reqBody)
@@ -49,9 +54,9 @@ const submitHandler = async request => {
 async function handleRequest(request) {
   const url = new URL(request.url)
 
-  if (url.pathname === "/submit") {
+  if (url.pathname === '/submit') {
     return submitHandler(request)
   }
 
-  return new Response.redirect(FORM_URL)
+  return Response.redirect(FORM_URL)
 }
